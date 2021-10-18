f1de7c9737d702a01282784cb2a4bd2025d4769a
DrKelvin <kelvinronald12@gmail.com>
2021-10-18 15:57:20 -0300

    Adicionado as paginas e rotas ja linkadas, para o usuario

--- resources/views/HunipHotel/about.blade.php
+++ resources/views/HunipHotel/about.blade.php
@@ -10,35 +10,16 @@
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
   <!--Style-->
   <link rel="stylesheet" href="{{url(mix('HunipHotel/style.css'))}}">
+  <title>Hunip Hotel Sobre Nós</title>
   <style>
     #navigation-bar{
       width: 100%;
       z-index: 100;
     }
-    #presentation h2{
-      font-family: 'Poppins';
-      font-weight: 800;
-      font-size: 3.5rem;
-      top: 18rem;
-      left: 4rem;
-    }
-    #presentation p{
-      font-family: 'Poppins';
-      font-weight: 500;
-      font-size: .9rem;
-      top: 27rem;
-      left: 5rem;
-    }
-    #imagem-hotel{
-      z-index: -1;
-      height: 100vh;
-      width: 100vw;
-    }
   </style>
-  <title>Hunip Hotel</title>
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark position-fixed" id="navigation-bar">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark position-fixed" id="navigation-bar">
     <div class="container">
       <a href="{{route('home')}}" class="navbar-brand" style="font-family:'Poppins';font-weight:800;font-size:1rem;">HUNIP <span style="color:#B8891F">HOTEL</span></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-label="Toggle navigation">
@@ -47,13 +28,13 @@
       <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
         <ul class="navbar-nav justify-content-around">
           <li class="nav-item">
-            <a href="{{route('home')}}" class="nav-link active" target="_self" style="font-family:'Poppins';font-weight:500;font-size:1rem;">HOME</a>
+            <a href="{{route('home')}}" class="nav-link" target="_self" style="font-family:'Poppins';font-weight:500;font-size:1rem;">HOME</a>
           </li>
           <li class="nav-item">
             <a href="{{route('quartos')}}" class="nav-link" target="_self" style="font-family:'Poppins';font-weight:500;font-size:1rem;">QUARTOS</a>
           </li>
           <li class="nav-item">
-            <a href="{{route('about')}}" class="nav-link" target="_self" style="font-family:'Poppins';font-weight:500;font-size:1rem;">SOBRE-NÓS</a>
+            <a href="{{route('about')}}" class="nav-link active" target="_self" style="font-family:'Poppins';font-weight:500;font-size:1rem;">SOBRE-NÓS</a>
           </li>
           <li class="nav-item">
             <a href="{{route('reservas')}}" class="nav-link" target="_self" style="font-family:'Poppins';font-weight:500;font-size:1rem;">RESERVAS</a>
@@ -64,15 +45,7 @@
     </div>
     
   </nav>
-  <main>
-    <section id="presentation">
-      <img id="imagem-hotel" src="{{asset('images/hotel-photo.png')}}" alt="imagem de frente do hotel" class="img-fluid position-absolute">
-      <h2 class="text-white position-absolute">O MELHOR HOTEL<br>DO MUNDO</h2>
-      <p class="text-white position-absolute">Esse hotel faz parte de um projeto da faculdade<br> logo não existe</p>
-    </section>
-    
-  </main>
-  <script src="{{url(mix('HunipHotel/jquery.js'))}}"></script>
+  
   <script src="{{url(mix('HunipHotel/bootstrap.js'))}}"></script>
 </body>
 </html>
