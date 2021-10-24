c6bae5e32e819db4dfb0550c7e02f7151ef57477
Gabriel Marinho <biel32771@gmail.com>
2021-10-24 11:40:37 -0300

    Criação migration dependentes

---
+++ app/Models/Dependente.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Dependente extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'cpf',
+        'idade',
+        'id_hospede',
+        'status'
+    ];
+
+}
---
+++ database/migrations/2021_10_24_134745_create_dependentes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDependentesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dependentes', function (Blueprint $table) {
+            $table->id();
+            $table->String('nome');
+            $table->String('cpf');
+            $table->Integer('idade');
+            $table->unsignedBigInteger('id_hospede');
+            $table->foreign('id_hospede')->references('id')->on('hospedes');
+            $table->String('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dependentes');
+    }
+}
