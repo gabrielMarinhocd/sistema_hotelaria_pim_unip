7a9fcf5360dbd39a31e68355b2494cb226ee2aca
Gabriel Marinho <biel32771@gmail.com>
2021-10-31 09:26:03 -0300

    Criação middleware

---
+++ app/Http/Middleware/AdminACL.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AdminACL
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+
+        if(auth()->check() && auth()->user()->admin){
+            return $next($request);
+        } dd('Sem acesso de admin');
+
+    }
+}
---
+++ app/Http/Middleware/HospedeACL.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class HospedeACL
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+    if(auth()->check() && auth()->user()->hospede){
+    return $next($request);
+    } dd('Sem acesso de hospede');
+    }
+}
